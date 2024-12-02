f76ab0ccfd447e0032d1bc398247c5072eaa9510
cmonpalo <rapcrisram326@gmail.com>
2024-12-02 02:41:57 -0500

    Started Nodes of Floor One, Taken Scripts from Pfeil Video

---
+++ Assets/NodeLoadingScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeLoadingScript : MonoBehaviour
+{
+    public Material myMaterial;
+    public GameObject myNodeList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
